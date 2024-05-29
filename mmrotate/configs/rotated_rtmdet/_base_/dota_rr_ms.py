38649734145bba49592736cf0c486e7f86015b3a
ahagai <amir.hagai@mail.huji.ac.il>
2024-05-29 09:38:54 +0000

    change size

--- mmrotate/configs/rotated_rtmdet/_base_/dota_rr_ms.py
+++ mmrotate/configs/rotated_rtmdet/_base_/dota_rr_ms.py
@@ -3,11 +3,24 @@ dataset_type = 'DOTADataset'
 data_root = 'data/split_ms_dota/'
 backend_args = None
 
+import sys 
+sys.path.append('/mm_stuff')
+# custom_imports = dict(imports=['converters.converter1'], allow_failed_imports=False)
+# conv = dict(type='Converter1', a=5, b=6)
+custom_imports = dict(imports=['transform.transforms'], allow_failed_imports=False)
+
+
+
+
+
+size = 512
+
 train_pipeline = [
     dict(type='mmdet.LoadImageFromFile', backend_args=backend_args),
     dict(type='mmdet.LoadAnnotations', with_bbox=True, box_type='qbox'),
     dict(type='ConvertBoxType', box_type_mapping=dict(gt_bboxes='rbox')),
-    dict(type='mmdet.Resize', scale=(1024, 1024), keep_ratio=True),
+    dict(type='BboxColorJitter2', prob=.8),
+    dict(type='mmdet.Resize', scale=(size, size), keep_ratio=True),
     dict(
         type='mmdet.RandomFlip',
         prob=0.75,
@@ -18,18 +31,18 @@ train_pipeline = [
         angle_range=180,
         rect_obj_labels=[9, 11]),
     dict(
-        type='mmdet.Pad', size=(1024, 1024),
+        type='mmdet.Pad', size=(size, size),
         pad_val=dict(img=(114, 114, 114))),
     dict(type='mmdet.PackDetInputs')
 ]
 val_pipeline = [
     dict(type='mmdet.LoadImageFromFile', backend_args=backend_args),
-    dict(type='mmdet.Resize', scale=(1024, 1024), keep_ratio=True),
+    dict(type='mmdet.Resize', scale=(size, size), keep_ratio=True),
     # avoid bboxes being resized
     dict(type='mmdet.LoadAnnotations', with_bbox=True, box_type='qbox'),
     dict(type='ConvertBoxType', box_type_mapping=dict(gt_bboxes='rbox')),
     dict(
-        type='mmdet.Pad', size=(1024, 1024),
+        type='mmdet.Pad', size=(size, size),
         pad_val=dict(img=(114, 114, 114))),
     dict(
         type='mmdet.PackDetInputs',
@@ -38,9 +51,9 @@ val_pipeline = [
 ]
 test_pipeline = [
     dict(type='mmdet.LoadImageFromFile', backend_args=backend_args),
-    dict(type='mmdet.Resize', scale=(1024, 1024), keep_ratio=True),
+    dict(type='mmdet.Resize', scale=(size, size), keep_ratio=True),
     dict(
-        type='mmdet.Pad', size=(1024, 1024),
+        type='mmdet.Pad', size=(size, size),
         pad_val=dict(img=(114, 114, 114))),
     dict(
         type='mmdet.PackDetInputs',
